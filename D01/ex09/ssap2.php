971ea2bfceb4cb06257b68116b37b1ce3c648dab
Florian MADURA <madura.florian@gmail.com>
2018-03-27 11:03:45 +0200

    nettoyage

--- D01/ex09/ssap2.php
+++ D01/ex09/ssap2.php
@@ -1,10 +1,14 @@
 #!/usr/bin/php
 <?php
+function print_tab($var)
+{
+	foreach($var as $print)
+		echo $print . "\n";
+}
 asort($argv);
 if (count($argv) > 1)
 {
 	$tab;
-	var_dump($argv);
 	foreach($argv as $key => $val)
 	{
 		if ($key != 0)
@@ -15,13 +19,18 @@ if (count($argv) > 1)
 		foreach($val as $word)
 		$return[] = $word;
 	natcasesort($return);
-	$pop;
-	foreach($return as $value)
-	{
-		if(ctype_alnum($value[0]))
-			$pop[]		
-	}
+	$num; $alpha; $oth;
 	foreach($return as $print)
-		echo $print . "\n";
+	{
+		if (ctype_digit($print[0]))
+			$num[]=$print;
+		else if (ctype_alpha($print[0]))
+			$alpha[]=$print;
+		else
+			$oth[]=$print;
+	}
+	print_tab($alpha);
+	print_tab($num);
+	print_tab($oth);
 }
 ?>
